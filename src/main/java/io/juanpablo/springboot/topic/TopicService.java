22fd3c0b680b472fe74fd37a9baef02e3019d956
Juan Pablo <juanpabloguirado@gmail.com>
2021-03-04 18:28:38 -0300

    Updated TopicService CRUD operations and application.properties

--- src/main/java/io/juanpablo/springboot/topic/TopicService.java
+++ src/main/java/io/juanpablo/springboot/topic/TopicService.java
@@ -1,48 +1,40 @@
 package io.juanpablo.springboot.topic;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TopicService {
 	
-	//We have to enclose the Arrays.asList as a parameter for a new ArrayList otherwise it will return
-	//an immutable object which will throw an error 
-	private List<Topic> topics = new ArrayList<>(Arrays.asList(
-			new Topic("java", "Java Core", "Basic Java Tools"),
-			new Topic("javascript", "Basic JS", "Getting started with JS"),
-			new Topic("sql", "DB SQL", "Getting started with databases")
-			));
+	@Autowired
+	private TopicRepository topicRepository;
 	
 	public List<Topic> getAllTopics(){
+		List<Topic> topics = new ArrayList<>();
+		//The method findAll returns an iterable, then for each element we are 'adding' it to 'topics'
+		topicRepository.findAll().forEach(topics::add);
 		return topics;
 	}
 	
 	public Topic getTopic(String id){
-		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+		return topicRepository.findById(id).orElse(null); 	
 	}
 	
 	//POST 
 	public void addTopic(Topic topic) {
-		topics.add(topic);
+		topicRepository.save(topic); 
 	}
 	
 	//PUT
 	public void updateTopic(String id, Topic topic) {
-		for(int i=0; i<topics.size(); i++) {
-			Topic t = topics.get(i);
-			if(t.getId().equals(id)) {
-				topics.set(i, topic);
-				return;
-			}
-		}	
+		topicRepository.save(topic);	
 	}
 	
 	//DELETE
 	public void deleteTopic(String id) {
-		topics.removeIf(t -> t.getId().equals(id));
+		topicRepository.deleteById(id);
 	}
 }
